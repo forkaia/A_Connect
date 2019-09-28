ad41dca3fb8925beb4c0c5151e6185e9704346c8
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-09-28 12:32:21 -0700

    added react router dom with router routes and links to separate components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from './pages/Home'
+import Info from './pages/Info'
 import './App.css';
 
 function App() {
-  return (
-    <Fragment><Home /></Fragment>
-  )
+    return (
+        <Fragment>
+            <Router>
+                <Route path='/' exact={true}><Home /></Route>
+            </Router>
+            <Router>
+                <Route path='/info' exact={true}><Info /></Route>
+            </Router>
+        </Fragment>
+    )
 }
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap'
+import { Button, Col, Row } from 'reactstrap'
 import { FaAngleDown } from "react-icons/fa"
+import { Link } from "react-router-dom";
 import homeBg from '../../images/Home/bg-home.jpg'
 import './home.css'
 console.log(`${homeBg} testing`)
@@ -12,67 +13,72 @@ class Home extends Component {
                 this.state = {}
         }
         render() {
-                return (<div
-                        id='home-bg'
-                        className='d-flex justify-content-center'
-                ><div
-                        className='text-center mx-auto'
-                        style={{
-
-                                backgroundImage: `url(${homeBg})`,
-                                /* Full height */
-                                // height: "100%",
-
-                                /* Center and scale the image nicely */
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                backgroundSize: 'cover',
-                                height: '650px',
-                                width: '100%'
-                        }}>
-                                <h1
-                                        className='text-white'
-                                        style={{
-                                                textAlign: 'center',
-                                                paddingTop: '8rem'
-                                        }}>
-                                        A-CONNECT
-                                </h1>
-                                <h4 className='text-white '>Take your student-organization to the next level</h4>
-                                <div>
-                                        <Button className='text-white home-buttons' color="primary">Get Started</Button>
-                                </div>
-                                <div className='mt-2'>
-                                        <Button className='home-buttons bg-white text-dark'>Log in</Button>
-                                </div>
-                                <div
-                                        className='d-flex justify-content-end flex-column-reverse'
-                                        style={{
-                                                marginTop: '20%'
-                                        }}
-                                >
-                                        <div>
-                                                <strong>
-                                                        <p className='text-white'>LEARN MORE</p>
-                                                </strong>
-                                        </div>
-                                        <div>
-                                                <button className='bg-none'
+                return (
+                        <Row>
+                                <Col>
+                                        <div
+                                                id='home-bg'
+                                                className='d-flex justify-content-center'>
+                                                <div
+                                                        className='text-center mx-auto'
                                                         style={{
-                                                                background: 'none',
-                                                                border: 'none',
 
-                                                        }}><FaAngleDown
+                                                                backgroundImage: `url(${homeBg})`,
+                                                                /* Full height */
+                                                                // height: "100%",
+
+                                                                /* Center and scale the image nicely */
+                                                                backgroundPosition: 'center',
+                                                                backgroundRepeat: 'no-repeat',
+                                                                backgroundSize: 'cover',
+                                                                height: '650px',
+                                                                width: '100%'
+                                                        }}>
+                                                        <h1
                                                                 className='text-white'
                                                                 style={{
-                                                                        width: '2rem',
-                                                                        height: '2rem'
-                                                                }} />
-                                                </button>
-                                        </div>
-                                </div>
-                        </div>
-                </div>);
+                                                                        textAlign: 'center',
+                                                                        paddingTop: '8rem'
+                                                                }}>
+                                                                A-CONNECT
+                                </h1>
+                                                        <h4 className='text-white '>Take your student-organization to the next level</h4>
+                                                        <div>
+                                                                <Button className='text-white home-buttons' color="primary">Get Started</Button>
+                                                        </div>
+                                                        <div className='mt-2'>
+                                                                <Button className='home-buttons bg-white text-dark'>Log in</Button>
+                                                        </div>
+                                                        <div
+                                                                className='d-flex justify-content-end flex-column-reverse'
+                                                                style={{
+                                                                        marginTop: '20%'
+                                                                }}
+                                                        >
+                                                                <div>
+                                                                        <strong>
+                                                                                <p className='text-white'>LEARN MORE</p>
+                                                                        </strong>
+                                                                </div>
+                                                                <div>
+                                                                        <Link to='/info'>
+                                                                                <button className='bg-none'
+                                                                                        style={{
+                                                                                                background: 'none',
+                                                                                                border: 'none',
+
+                                                                                        }}><FaAngleDown
+                                                                                                className='text-white'
+                                                                                                style={{
+                                                                                                        width: '2rem',
+                                                                                                        height: '2rem'
+                                                                                                }} />
+                                                                                </button>
+                                                                        </Link>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div></Col></Row>);
         }
 }
 
