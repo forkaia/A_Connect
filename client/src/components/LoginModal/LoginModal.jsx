d9f57941ad5d7c523a68b6e8b7b4f6205259373d
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-10-03 09:12:52 -0700

    added a handlesubmit function for the login modal that takes the users username and password from client and then run inputs through the backend controller function to log in and returns a jwt if successful or an error message if not successful

--- client/src/components/LoginModal/LoginModal.jsx
+++ client/src/components/LoginModal/LoginModal.jsx
@@ -26,17 +26,40 @@ class LoginModal extends Component {
 
         handleSubmit = (e) => {
                 e.preventDefault();
-                console.log('Submitted')
+                const { username, password } = this.state
+                const loginInfo = {
+                        username,
+                        password
+                }
+                fetch('/users/login', {
+                        method: "POST",
+                        headers: {
+                                "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(loginInfo)
+
+                }).then((res) => res.json()).then((data) => {
+                        console.log(data)
+                        localStorage.setItem("token", data.token)
+                }).catch((err) => {
+                        if (err) {
+                                alert("Incorrect username/email entry or password")
+                        }
+                })
+
         }
 
         render() {
                 return (
                         <div>
                                 <Button onClick={this.toggle} className='home-buttons bg-white text-dark'>Log in</Button>
+
                                 <Modal isOpen={this.state.modal} toggle={this.toggle} className='d-flex mx-auto justfiy-content-center text-center'>
-                                        <ModalHeader toggle={this.toggle}>Log In to Your Account</ModalHeader>
-                                        <ModalBody>
-                                                <form>
+                                        <form onSubmit={this.handleSubmit}>
+                                                <ModalHeader toggle={this.toggle}>Log In to Your Account</ModalHeader>
+
+                                                <ModalBody>
+
                                                         <div>
                                                                 <label className='label'>Username or Email: </label>
                                                                 <input type='text' name='username' onChange={this.handleChange} />
@@ -45,13 +68,16 @@ class LoginModal extends Component {
                                                                 <label className='label'>Password: </label>
                                                                 <input type='text' name='password' onChange={this.handleChange} />
                                                         </div>
-                                                </form>
-                                        </ModalBody>
-                                        <ModalFooter className='d-flex mx-auto justfiy-content-center text-center'>
-                                                <input onSubmit={this.handleSubmit} type='submit' className='btn btn-secondary' />
-                                                <Button onClick={this.toggle}>Cancel</Button>
-                                        </ModalFooter>
+
+                                                </ModalBody>
+                                                <ModalFooter className='d-flex mx-auto justfiy-content-center text-center'>
+                                                        <input type='submit' className='btn btn-secondary' />
+                                                        <Button onClick={this.toggle}>Cancel</Button>
+                                                </ModalFooter>
+                                        </form>
+
                                 </Modal>
+
                         </div>
                 );
         }
